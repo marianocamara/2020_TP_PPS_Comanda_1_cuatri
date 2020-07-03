943a442af21451939a9906013eeee9e94ab3df1c
marianocamara <camaramariano@gmail.com>
2020-07-02 22:23:08 -0300

    splas

--- morfy/src/app/models/survey.ts
+++ morfy/src/app/models/survey.ts
@@ -12,17 +12,9 @@ export class Survey {
 }
 
 export enum SurveyQuestionsUser {
-    Overall_Score = 'Selecciona el grado de satisfaccion que represente tu experiencia en nuestro restaurante.',
-    Would_Recommend = 'Recomendarias nuestro restarurante a algun conocido?',
-    Best_Quality = 'Cual dirias que es nuestra mejor cualidad?',
-    Could_Improve = 'Cuales dirias que son los aspectos que debemos mejorar de nuestro restaurante?',
-    Comments = 'Quieres contarnos algo?'
-}
-
-export enum SurveyQuestionsEmployee {
-    Overall_Score = 'Selecciona el grado de satisfaccion que represente tu experiencia en nuestro restaurante.',
-    Would_Recommend = 'Recomendarias nuestro restarurante a algun conocido?',
-    Best_Quality = 'Cual dirias que es nuestra mejor cualidad?',
-    Could_Improve = 'Cuales dirias que son los aspectos que debemos mejorar de nuestro restaurante?',
+    Overall_Score = 'Selecciona el grado de satisfacción que represente tu experiencia en nuestro restaurante.',
+    Would_Recommend = 'Recomendarías nuestro restarurante a algún conocido?',
+    Best_Quality = 'Cuál dirías que es nuestra mejor cualidad?',
+    Could_Improve = 'Cuales dirías que son los aspectos que debemos mejorar de nuestro restaurante?',
     Comments = 'Quieres contarnos algo?'
 }
